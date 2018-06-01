ce6351b50ed5a138cc738ed5b5e16d0f5f7411cf
jonathanbain1992 <2092528b@student.gla.ac.uk>
2018-06-01 02:31:42 +0100

    defined some new models, fixed the format of the events (for now, imperfect)- need to fix the models as they aren't defined correctly (did not have docs available at time of writing

--- campusapp/models.py
+++ campusapp/models.py
@@ -14,3 +14,38 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+class Promotion(models.Model):
+    #put in offer ID and times redeemed field.
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+    valid_from = models.DateTimeField()
+    valid_until = models.DateTimeField()
+    image = models.ImageField(default=None)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=30)
+    description =models.TextField(max_length=200)
+    price = models.FloatField(default=00.00)
+    image = models.ImageField(default=None)
+
+    def __str__(self):
+        return self.name
+
+class FoodItem(models.Model):
+    MenuItem = models.OneToOneField(MenuItem)  #extends Menu Item (does this work?)
+    SideItem = models.OneToManyField(SideItem) #might need to change order of this as SideItem defined afterwards
+
+    def __str__(self):
+        return self.name
+
+class SideItem(models.Model):
+    name = models.CharField(max_length=30)
+    Quantity = models.IntegerField(default = 0)
+    image = models.ImageField(default=None) #might need to remove this later - extending class with image FloatField
+
+    def __str__(self):
+        return self.name
